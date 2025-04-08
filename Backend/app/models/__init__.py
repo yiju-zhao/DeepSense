1399aea8a7d3d13b534f93f0415c2e0eebe01283
Eason <29761446+yiju-zhao@users.noreply.github.com>
2025-04-08 09:44:36 -0400

    solve conflict

--- Backend/app/models/__init__.py
+++ Backend/app/models/__init__.py
@@ -1,7 +1,8 @@
-from app.models.user import User
-from app.models.conference import Conference, ConferenceInstance
-from app.models.paper import Paper, Person, Organization, Keyword, Reference
-from app.models.session import Session
-from app.models.report import Report, ReportVersion, Notebook, NotebookSnapshot
+from user import User
+from conference import Conference, ConferenceInstance
+from paper import Paper, Person, Organization, Keyword, Reference
+from session import Session
+from report import Report, ReportVersion, Notebook, NotebookSnapshot
+from tasks import *
 
 # Import all models here to make them available when importing from app.models
