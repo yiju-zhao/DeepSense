aaa606e80a47834aedd4e90e390a3dd406861e6f
Eason <29761446+yiju-zhao@users.noreply.github.com>
2025-04-08 09:46:30 -0400

    solve conflict

--- Backend/app/api/__init__.py
+++ Backend/app/api/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import APIRouter
 from .routes.tasks import router as tasks_router
 from .routes.frontend_adapter import router as frontend_adapter_router
@@ -6,10 +5,3 @@ from .routes.frontend_adapter import router as frontend_adapter_router
 api_router = APIRouter()
 api_router.include_router(tasks_router)
 api_router.include_router(frontend_adapter_router)
-=======
-from app.api.routes import api_router
-from app.api.errors import add_error_handlers
-from app.api.middleware import add_logging_middleware
-
-# Import all API components here to make them available when importing from app.api
->>>>>>> origin/main
